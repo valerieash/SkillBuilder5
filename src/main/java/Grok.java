87137c1e5dab8e94b755f0f6f40b0aed24b5e027
valerieash <valerieash4@gmail.com>
2025-04-01 23:32:28 -0400

    Complete

--- src/main/java/Grok.java
+++ src/main/java/Grok.java
@@ -9,5 +9,50 @@
  */
 public class Grok
 {
-    // implement this class per instructions in Required Activities.
+    private static final int DEFAULT_POWER_LEVEL = 50;
+    private int powerLevel;
+    /**
+     \* Initializes a Grok object to the default power level of 50.
+     */
+
+    public Grok(){
+        this.powerLevel= DEFAULT_POWER_LEVEL;
+    }
+    /**
+     *
+     * Initializes a Grok object to power powerLevel
+     */
+    public Grok(int powerLevel){
+        this.powerLevel = powerLevel;
+    }
+    /**
+     * Returns the power level of this Grok.
+     * @return returns the power level of this Grok
+     */
+    public int getPowerLevel(){
+        return powerLevel;
+    }
+
+    /**
+     * Sets the power level of this Grok.
+     * @param powerLevel the power value to set for this Grok.
+     */
+
+    public void setPowerLevel(int powerLevel){
+        this.powerLevel = powerLevel;
+    }
+
+
+    public void takePowerPill(PowerPill pill){
+        this.powerLevel += pill.getPower();
+    }
+    /**
+     * Invoked when this Grok takes a hit.  The power level of
+     * this Grok is reduced by 5.
+     */
+    public void tookHit(){
+        this.powerLevel -= 5;
+    }
+
+
 }
--- src/main/java/PowerPill.java
+++ src/main/java/PowerPill.java
@@ -7,32 +7,50 @@
  * @author (You)
  * @version (0.1)
  */
-public class PowerPill
-{
-    // TODO - replace this line with instruction from step 1
+public class PowerPill {
+    public static int DEFAULT_POWER = 10;
+
+    int power;
+    String name;
 
     // instance variables
-    // TODO - replace this line with instruction from step 2
-    // TODO - replace this line with instruction from step 3
-
-    // constructors
-
-    // TODO - replace this line with instruction from step 4
-
-    // TODO - replace this line with instruction from step 5
-
-
+    public PowerPill(String name){
+        this.name = name;
+        this.power = DEFAULT_POWER;
+    }
+    /**
+     * Initializes this power pill to the value of power
+     * and sets the name of the pill to name.
+     * @param name the name of this power pill
+     * @param power the power level of this power pill.
+     */
+    public PowerPill(String name, int power) {
+        this.name = name;
+        this.power = power;
+    }
     // accessor methods
-
-    // TODO - replace this line with instruction from step 6
-
+    public String getName() {
+        return name;
+    }
+    public int getPower(){
+        return power;
+    }
 
     // mutator methods
 
-    // TODO - replace this line with instruction from step 7
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setPower(int power){
+        this.power = power;
+    }
 
     // toString method
 
-    // TODO - replace this line with instruction from step 8
+    public String toString(){
+        return "PowerPill" + name + " = " + power;
+    }
 
 }
+
